e3ff9d8b0052da3b23fabe121abd4f19d4a3d302
varshameghana <meghana130@gmail.com>
2019-02-28 00:01:35 +0530

    dist path commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/deploy-fe'));
+app.use(express.static(__dirname + '/dist/radius-display-issues'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/deploy-fe/index.html'));
+res.sendFile(path.join(__dirname+'/dist/radius-display-issues/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
